fa2a8572681633554347c98bb8ada00e78df8a45
ice22d <derbratan1232@gmail.com>
2025-12-05 07:56:41 +0100

    Update loader.user.js

--- loader.user.js
+++ loader.user.js
@@ -6,7 +6,7 @@
 // @match        *://damnbruh.com/*
 // @match        *://www.damnbruh.com/*
 // @grant        none
-// @require      https://cdn.jsdelivr.net/gh/ice22d/dbmonitor@main/main.obf.js?v=2
+// @require      https://cdn.jsdelivr.net/gh/ice22d/dbmonitor@main/main.safe.obf.js
 // @run-at       document-start
 // ==/UserScript==
 /* Minimal loader — the actual logic is in the required script. */
