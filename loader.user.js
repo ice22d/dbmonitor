b60d3e99cf9f47910e238bf02b8c384dcaa7b1e3
ice22d <derbratan1232@gmail.com>
2025-12-05 07:47:32 +0100

    Update script URL with version query parameter

--- loader.user.js
+++ loader.user.js
@@ -6,7 +6,7 @@
 // @match        *://damnbruh.com/*
 // @match        *://www.damnbruh.com/*
 // @grant        none
-// @require      https://cdn.jsdelivr.net/gh/ice22d/dbmonitor@main/main.obf.js
+// @require      https://cdn.jsdelivr.net/gh/ice22d/dbmonitor@main/main.obf.js?v=2
 // @run-at       document-start
 // ==/UserScript==
 /* Minimal loader — the actual logic is in the required script. */
