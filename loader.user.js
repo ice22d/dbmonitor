00dbb01dadf5c678738512e236337a801c9bade5
ice22d <derbratan1232@gmail.com>
2025-12-05 07:34:48 +0100

    Update script URL in loader.user.js

--- loader.user.js
+++ loader.user.js
@@ -6,7 +6,7 @@
 // @match        *://damnbruh.com/*
 // @match        *://www.damnbruh.com/*
 // @grant        none
-// @require      REQUIRE_URL
+// @require      https://cdn.jsdelivr.net/gh/ice22d/dbmonitor@main/main.obf.js
 // @run-at       document-start
 // ==/UserScript==
 /* Minimal loader — the actual logic is in the required script. */
