07f6d3c42757f79e171acc615e6ffa4467e8cc0e
ice22d <derbratan1232@gmail.com>
2025-12-05 08:40:26 +0100

    Update loader.user.js

--- loader.user.js
+++ loader.user.js
@@ -2,11 +2,9 @@
 // @name         DamnBruhServerMonitorV2 Loader
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  Loader for external DamnBruh monitor script.
 // @match        *://damnbruh.com/*
 // @match        *://www.damnbruh.com/*
 // @grant        none
-// @require      https://cdn.jsdelivr.net/gh/ice22d/dbmonitor@main/main.obf.js
+// @require      https://raw.githubusercontent.com/ice22d/dbmonitor/main/main.obf.js
 // @run-at       document-start
 // ==/UserScript==
-/* Minimal loader — the actual logic is in the required script. */
